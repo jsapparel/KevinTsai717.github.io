912d44410935b876794b0f5be0bd7ddd3db0bca4
Kevintsai717 <59856420+Kevintsai717@users.noreply.github.com>
2020-03-09 17:50:05 +1100

    09032020

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,3 +41,11 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+var cursor = document.getElementById('cursor');
+document.addEventListener('mousemove',function(e){
+	var x = e.clientX;
+	var y = e.clientY;
+	cursor.style.left = x + "px";
+	cursor.style.top = y + "px";
+});
